9ea7498ca2c38b1fa7e1d6f17b52e6f23bdce3f8
RomanMereneanu <asasin84@mail.ru>
2021-01-01 12:23:09 +0200

    add auth

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -7,7 +7,9 @@ function Example() {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Success</div>
+                        <div className="card-header">Example Component</div>
+
+                        <div className="card-body">I'm an example component!</div>
                     </div>
                 </div>
             </div>
